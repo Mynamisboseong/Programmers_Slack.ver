cec6c6cb6c46d6fd0592cd40290967b4e197f8cd
Mynamisboseong <wow456869@gmail.com>
2024-08-19 20:47:47 +0900

    form태그사용

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const $app = document.getElementById("app");
 
 const $header = document.createElement("h1");
-$header.textContent = "안녕하세요!!!"
+$header.textContent = "오늘의 Todo_List"
 
 $app.appendChild($header);
 
-//input과 button만들기
-const $input = document.createElement("input")
-const $button = document.createElement("button")
-$app.appendChild($input)
-$app.appendChild($button)
-$button.innerText = "입력"
-
-//list만들기
-const $ul = document.createElement("ul")
-$app.appendChild($ul)
-const $li = document.createElement("li")
-$ul.appendChild($li)
- 
-$li.textContent = "리스트 만들기"
-
+//list만들기(innetHTML이용)
+$form = document.createElement("form")
+$app.appendChild($form)
+$form.innerHTML = `
+    <input/><button>입력</button>
+    <ul>
+        <li>할일1</li>
+        <li>할일2</li>
+        <li>할일3</li>
+        <li>할일4</li>
+    </ul>
+`
 
