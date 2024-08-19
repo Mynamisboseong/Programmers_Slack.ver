c7d26e5b60fabf44c0132ef797719379548a80c0
Mynamisboseong <wow456869@gmail.com>
2024-08-19 20:36:28 +0900

    기초적인운용

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const $header = document.createElement("h1");
 $header.textContent = "안녕하세요!!!"
 
 $app.appendChild($header);
+
+//input과 button만들기
+const $input = document.createElement("input")
+const $button = document.createElement("button")
+$app.appendChild($input)
+$app.appendChild($button)
+$button.innerText = "입력"
+
+//list만들기
+const $ul = document.createElement("ul")
+$app.appendChild($ul)
+const $li = document.createElement("li")
+$ul.appendChild($li)
+ 
+$li.textContent = "리스트 만들기"
+
+
